666bf4faf7c055e54c63923d9a58996f43358bfe
Alan O <alan@buyhouse.co.uk>
2012-10-31 10:25:22 +0000

    First version

---
+++ jquery.rotateX.js
@@ -0,0 +1,220 @@
+﻿/***********************************************************
+* Rotate 
+* Alan Oliver
+* Modifications:-
+*  Version 1 30-October-2012
+*   - return jQuery objects (allows to change opacity etc)
+*   - make test for IE more robust
+*   - doesn't work in IE9, don't try CSS for any IE version
+************************************************************
+* Originally based upon jquery.rotate by Wilq32 (see below)
+* (VERSION: 3.1 LAST UPDATE: 13.03.2012)
+***********************************************************/
+/* 
+* Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+* 
+* Made by Wilq32, wilq32@gmail.com, Wroclaw, Poland, 01.2009
+* Website: http://code.google.com/p/jqueryrotate/ 
+*/
+
+(function ($) {
+    var supportedCSS, styles = document.getElementsByTagName("head")[0].style;
+    var toCheck = "transformProperty WebkitTransform OTransform msTransform MozTransform".split(" "); //MozTransform <- firefox works slower with css!!!
+    for (var a = 0; a < toCheck.length; a++) if (styles[toCheck[a]] !== undefined) supportedCSS = toCheck[a];
+    var IE = Boolean(navigator.appVersion.indexOf('MSIE')+1);
+
+    jQuery.fn.extend({
+        rotate: function (options) {
+            if (this.length === 0 || typeof options == "undefined") return;
+            if (typeof options == "number") options = { angle: options };
+            var $newRotObjects = $();
+            for (var i = 0, i0 = this.length; i < i0; i++) {
+                var element = this.get(i);
+                if (!element.Wilq32 || !element.Wilq32.PhotoEffect) {
+
+                    var optionsClone = $.extend(true, {}, options);
+                    var newRotObject = new Wilq32.PhotoEffect(element, optionsClone)._rootObj;
+
+                    $newRotObjects = $newRotObjects.add(newRotObject);
+                }
+                else {
+                    element.Wilq32.PhotoEffect._handleRotation(options);
+                }
+            }
+            return $newRotObjects;
+        },
+        getRotateAngle: function () {
+            var ret = [];
+            for (var i = 0, i0 = this.length; i < i0; i++) {
+                var element = this.get(i);
+                if (element.Wilq32 && element.Wilq32.PhotoEffect) {
+                    ret[i] = element.Wilq32.PhotoEffect._angle;
+                }
+            }
+            return ret;
+        },
+        stopRotate: function () {
+            for (var i = 0, i0 = this.length; i < i0; i++) {
+                var element = this.get(i);
+                if (element.Wilq32 && element.Wilq32.PhotoEffect) {
+                    clearTimeout(element.Wilq32.PhotoEffect._timer);
+                }
+            }
+        }
+    });
+
+    // Library agnostic interface
+
+    Wilq32 = window.Wilq32 || {};
+    Wilq32.PhotoEffect = (function () {
+
+        if (IE) {
+            return function (img, options) {
+                // Make sure that class and id are also copied - just in case you would like to refeer to an newly created object
+                this._img = img;
+
+                this._rootObj = document.createElement('span');
+                this._rootObj.style.display = "inline-block";
+                this._rootObj.Wilq32 = { PhotoEffect: this };
+                img.parentNode.insertBefore(this._rootObj, img);
+                this._Loader(options);
+            }
+        } else if (supportedCSS) {
+            return function (img, options) {
+                img.Wilq32 = { PhotoEffect: this };
+
+                this._img = this._rootObj = this._eventObj = img;
+                this._handleRotation(options);
+            }
+        } else {
+            return function (img, options) {
+                // Just for now... Dont do anything if CSS3 is not supported
+                this._rootObj = img;
+            }
+        }
+    })();
+
+    Wilq32.PhotoEffect.prototype = {
+        _setupOptions: function (options) {
+            this._options = this._options || {};
+            if (typeof this._angle !== "number") this._angle = 0;
+            if (typeof options.angle === "number") this._angle = options.angle;
+            this._options.animateTo = (typeof options.animateTo === "number") ? (options.animateTo) : (this._angle);
+
+            this._options.step = options.step || this._options.step || null;
+            this._options.easing = options.easing || this._options.easing || function (x, t, b, c, d) { return -c * ((t = t / d - 1) * t * t * t - 1) + b; }
+            this._options.duration = options.duration || this._options.duration || 1000;
+            this._options.callback = options.callback || this._options.callback || function () { };
+            if (options.bind && options.bind != this._options.bind) this._BindEvents(options.bind);
+        },
+        _handleRotation: function (options) {
+            this._setupOptions(options);
+            if (this._angle == this._options.animateTo) {
+                this._rotate(this._angle);
+            }
+            else {
+                this._animateStart();
+            }
+        },
+
+        _BindEvents: function (events) {
+            if (events && this._eventObj) {
+                // Unbinding previous Events
+                if (this._options.bind) {
+                    var oldEvents = this._options.bind;
+                    for (var a in oldEvents) if (oldEvents.hasOwnProperty(a))
+                    // TODO: Remove jQuery dependency
+                        jQuery(this._eventObj).unbind(a, oldEvents[a]);
+                }
+
+                this._options.bind = events;
+                for (var b in events) if (events.hasOwnProperty(b))
+                // TODO: Remove jQuery dependency
+                    jQuery(this._eventObj).bind(b, events[b]);
+            }
+        },
+
+        _Loader: function (options) {
+            var width = this._img.width;
+            var height = this._img.height;
+            //this._img.parentNode.removeChild(this._img);
+            //this._rootObj.parentNode.removeChild(this._rootObj);
+
+            this._rootObj.appendChild(this._img);
+
+            this._rootObj.style.width = this._img.offsetWidth;
+            this._rootObj.style.height = this._img.offsetHeight;
+
+            this._img.style.position = "absolute";
+
+            this._rootObj = this._img;
+            this._rootObj.Wilq32 = { PhotoEffect: this }
+
+            this._rootObj.style.filter += "progid:DXImageTransform.Microsoft.Matrix(M11=1,M12=1,M21=1,M22=1,sizingMethod='auto expand')";
+
+            this._eventObj = this._rootObj;
+            this._handleRotation(options);
+        },
+
+        _animateStart: function () {
+            if (this._timer) {
+                clearTimeout(this._timer);
+            }
+            this._animateStartTime = +new Date;
+            this._animateStartAngle = this._angle;
+            this._animate();
+        },
+        _animate: function () {
+            var actualTime = +new Date;
+            var checkEnd = actualTime - this._animateStartTime > this._options.duration;
+
+            // TODO: Bug for animatedGif for static rotation ? (to test)
+            if (checkEnd && !this._options.animatedGif) {
+                clearTimeout(this._timer);
+            }
+            else {
+                if (this._canvas || this._vimage || this._img) {
+                    var angle = this._options.easing(0, actualTime - this._animateStartTime, this._animateStartAngle, this._options.animateTo - this._animateStartAngle, this._options.duration);
+                    this._rotate((~ ~(angle * 10)) / 10);
+                }
+                if (this._options.step) {
+                    this._options.step(this._angle);
+                }
+                var self = this;
+                this._timer = setTimeout(function () {
+                    self._animate.call(self);
+                }, 10);
+            }
+
+            // To fix Bug that prevents using recursive function in callback I moved this function to back
+            if (this._options.callback && checkEnd) {
+                this._angle = this._options.animateTo;
+                this._rotate(this._angle);
+                this._options.callback.call(this._rootObj);
+            }
+        },
+
+        _rotate: (function () {
+            var rad = Math.PI / 180;
+            if (IE)
+                return function (angle) {
+                    this._angle = angle;
+                    //this._container.style.rotation=(angle%360)+"deg";
+                    var _rad = angle * rad;
+                    costheta = Math.cos(_rad);
+                    sintheta = Math.sin(_rad);
+                    var fil = this._rootObj.filters.item("DXImageTransform.Microsoft.Matrix");
+                    fil.M11 = costheta; fil.M12 = -sintheta; fil.M21 = sintheta; fil.M22 = costheta;
+
+                    this._rootObj.style.marginLeft = -(this._rootObj.offsetWidth - this._rootObj.clientWidth) / 2 + "px";
+                    this._rootObj.style.marginTop = -(this._rootObj.offsetHeight - this._rootObj.clientHeight) / 2 + "px";
+                }
+            else if (supportedCSS)
+                return function (angle) {
+                    this._angle = angle;
+                    this._img.style[supportedCSS] = "rotate(" + (angle % 360) + "deg)";
+                }
+
+        })()
+    }
+})(jQuery);
---
+++ readme.md
@@ -0,0 +1,206 @@
+﻿jquery.rotateX
+==============
+
+Rotate images with optional animation and easing
+------------------------------------------------
+
+Based upon and directly replaces _jquery.rotate.js_ version 3.1, with one 
+important difference... the `.rotate()` method in this version returns a
+jQuery selection, making it easier to manipulate the rotating objects
+created by this plugin.
+
+# Usage
+
+Download the file _jquery.rotateX.js_ and place it in one of your web application's
+folders the add a reference in the <head> of each page in which it will be used.
+
+For example, if the file is in your website's "/scripts" folder, use this line:-
+
+	<script type="text/javascript" src="scripts/jquery.rotateX.js"></script>
+
+
+# Methods
+
+**Summary explanation for all supported methods, parameters and options**
+
+## rotate(angle)
+
+**Rotate the selected images**
+
+***Returns:*** jQuery selection of newly created rotating images
+
+***Parameters:***
+
+* **angle**
+
+	`[Number]` - _default 0 (zero)_ - Angle value in degrees, for immediate rotation
+
+	Example:
+
+		$("#img").rotate(45);
+
+
+## rotate(options)
+
+**Rotate the selected images, with optional animation and easing**
+
+***Returns:*** jQuery selection of newly created rotating images
+
+***Parameters:***
+
+* **options**
+
+	`[Object]` Object containing options for rotation. 
+	
+	***Supported options:***
+
+	* *angle*
+
+		`[Number]` - default _0 (zero)_ - Angle value in degrees, for immediate rotation
+
+		Example:
+
+			$("#img").rotate({angle:45});
+
+	* *bind*
+
+		`[Object]` Object containing events to bind on to a rotation object. $(this) inside events points to a rotation object - this way you can chain execution of rotations - $(this).rotate(...)
+
+		Example:
+
+			$("#img").rotate({bind:{
+				click: function(){
+					$(this).rotate({
+						angle: 0, 
+						animateTo: 180
+					});
+				}
+			} });
+
+	* *animateTo*
+
+		`[Number]` - default 0 _(zero)_ - Angle value in degrees, for rotation to be animated to from current angle value (or given angle option value)
+
+	    Example:
+	
+			(See bind example above for usage)
+
+	* *duration*
+
+		`[Number]` - default 1000 - Specifies a duration of animation, in milliseconds, when using animateTo action
+
+		Example:
+
+			$("#img").rotate({bind:{
+				click: function(){
+					$(this).rotate({
+						duration: 6000,
+						angle: 0, 
+						animateTo: 100
+					});
+				}
+			} });
+
+	* *step*
+
+		`[Function]` - default _null_ - A function that will be executed on every animation step.
+		
+		Arguments:
+
+			___currentAngle___ The current angle is provided as first argument.
+
+	* *easing*
+
+		`[Function]` - default (see below) - Easing function is used to make animation look more natural. 
+		
+		The function uses five parameters (x,t,b,c,d) to support easing from http://gsgd.co.uk/sandbox/jquery/easing/ (for more details please see documentation at their website). Remember to include easing plugin before using it in jQueryRotate!
+
+		Default function:
+
+			function (x, t, b, c, d) { return -c * ((t=t/d-1)*t*t*t - 1) + b; }
+
+		Where:
+			* t: current time, 
+			* b: begInnIng value,
+			* c: change In value, 
+			* d: duration,
+			* x: unused
+
+		No easing (linear easing): 
+
+			function(x, t, b, c, d) { return (t/d)*c ; }
+
+		Example:
+
+			$("#img").rotate({bind:{
+				click: function(){
+					$(this).rotate({
+						angle: 0, 
+						animateTo: 180,
+						easing: $.easing.easeInOutElastic
+					});
+				}
+			} });
+
+	* *callback*
+
+		`[Function]` A function to be called when animation finishes.
+
+		Example:
+
+			$("#img").rotate({bind:{
+				click: function(){
+					$(this).rotate({
+						angle: 0, 
+						animateTo: 180,
+						callback: function() { alert(1); }
+					});
+				}
+			} });
+
+
+## getRotateAngle
+
+**Get the current angle of the selected rotating objects**
+
+***Returns:*** Array of integers. The current angle of each selected 
+rotating object.
+
+***Parameters:*** (none)
+
+Example:
+
+	$("#img").rotate({
+		angle: 45, 
+        bind: {
+			click : function(){
+				alert($(this).getRotateAngle());
+            }
+        }
+	});
+
+## stopRotate
+ 
+**Stop rotation of the selected rotating object**
+
+***Returns:*** (nothing)
+
+***Parameters:*** (none)
+ 
+Example: 
+
+	$("#img").rotate({
+		bind: {
+			click: function(){
+				$("#img").rotate({
+					angle: 0,
+                    animateTo: 180,
+                    duration: 6000
+                });
+                setTimeout(function(){
+                    $("#img").stopRotate();
+                }, 1000);
+            }
+        }
+	});
+ 
